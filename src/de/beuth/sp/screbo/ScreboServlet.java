602b6375e7490db4c61946b0cea309601c544e60
Volker Gronau <Volker.Gronau@dimensiondata.com>
2016-06-23 20:39:36 +0200

    disable log4j on shutdown

--- src/de/beuth/sp/screbo/ScreboServlet.java
+++ src/de/beuth/sp/screbo/ScreboServlet.java
@@ -56,7 +56,6 @@ public class ScreboServlet extends VaadinServlet {
 		public void run() {
 			if (loggerContext != null) {
 				Configurator.shutdown(loggerContext);
-				loggerContext = null;
 			}
 		}
 	};
